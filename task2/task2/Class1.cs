1e5de691b981d54d074fbc0b45718215f2a769c1
Julia <julia.boichuk2105@gmail.com>
2019-05-17 19:32:05 +0300

    Task2 create tests

---
+++ task2/task2/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace task2
+{
+    [TestFixture]
+    public class Class1
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments
+                (
+                    "--start-maximized",
+                    "--disable-extensions",
+                    "--disable-notifications",
+                    "--disable-application-cache"
+                );
+
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("http://automationpractice.com");
+
+            IWebElement LoginButton = driver.FindElement(By.ClassName("login"));
+            LoginButton.Click();
+
+            IWebElement login = driver.FindElement(By.Name("email"));
+            login.SendKeys("testacount@gmail.com");
+
+            IWebElement pass = driver.FindElement(By.Name("passwd"));
+            pass.SendKeys("testacount");
+            
+            IWebElement auth = driver.FindElement(By.Name("SubmitLogin"));
+            auth.Click();
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+       
+        [Test]
+        public void CheckAccount()
+        {
+            var Account = driver.FindElement(By.Id("my-account"));
+            Account.Click();
+
+            Assert.AreEqual("My account - My Store", driver.Title);
+        }
+
+        [Test]
+        public void CheckAccountChange()
+        {
+            IWebElement InformButton = driver.FindElement(By.ClassName("icon-user"));
+            InformButton.Click();
+
+            IWebElement name = driver.FindElement(By.Name("firstname"));
+            name.Clear();
+            name.SendKeys("Julia");
+
+            IWebElement lname = driver.FindElement(By.Name("lastname"));
+            lname.Clear();
+            lname.SendKeys("Boichuk");
+
+            IWebElement oldPass = driver.FindElement(By.Name("old_passwd"));
+            oldPass.SendKeys("testacount");
+
+            IWebElement newPass = driver.FindElement(By.Name("passwd"));
+            newPass.SendKeys("testacount1");
+
+            IWebElement Confirm = driver.FindElement(By.Name("confirmation"));
+            Confirm.SendKeys("testacount1");
+
+            IWebElement SaveButton = driver.FindElement(By.Name("submitIdentity"));
+            SaveButton.Click();
+            
+            var User = driver.FindElement(By.ClassName("account"));
+            Assert.AreEqual("Julia Boichuk", User.Text);
+        }
+    }
+}
